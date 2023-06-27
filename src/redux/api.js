e8a6846e65943bd787f335fa5e4f2f6728f55628
andrianarivo <andrianarivo.david@gmail.com>
2023-06-27 16:52:49 +0300

    build get continents

---
+++ src/redux/api.js
@@ -0,0 +1,7 @@
+const baseUrl = 'https://disease.sh';
+
+const GET_CONTINENTS = `${baseUrl}/v3/covid-19/continents`;
+
+export { GET_CONTINENTS };
+
+export default baseUrl;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,33 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { selectContinent } from '../redux/store';
+import { getContinents } from '../redux/continents/continentsSlice';
+import { GET_CONTINENTS } from '../redux/api';
+
 export default function Home() {
+  const { loading, error, continentItems } = useSelector(selectContinent);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getContinents({ url: GET_CONTINENTS }));
+  }, [dispatch]);
+
+  if (loading) {
+    return (
+      <div>Loading...</div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div>{error}</div>
+    );
+  }
+
   return (
     <div>
       This is the home page
+      <pre>{JSON.stringify(continentItems, null, 2)}</pre>
     </div>
   );
 }
