66ae60dafbea773fce92b0be5efa268715e92aaa
Andrianarivo David <andrianarivo.david@gmail.com>
2023-06-27 21:32:28 +0300

    refactor

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,6 @@ import { NavLink } from 'react-router-dom';
 export default function NavBar() {
   const links = [
     { path: '/', name: 'Home' },
-    { path: 'details', name: 'Details' },
   ];
   return (
     <nav>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { selectContinent } from '../redux/store';
 import { getContinents } from '../redux/continents/continentsSlice';
 import { GET_CONTINENTS } from '../redux/api';
@@ -7,6 +8,7 @@ import { GET_CONTINENTS } from '../redux/api';
 export default function Home() {
   const { loading, error, continentItems } = useSelector(selectContinent);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   useEffect(() => {
     dispatch(getContinents({ url: GET_CONTINENTS }));
@@ -27,7 +29,29 @@ export default function Home() {
   return (
     <div>
       This is the home page
-      <pre>{JSON.stringify(continentItems, null, 2)}</pre>
+      <ul>
+        {continentItems.map((item) => (
+          <li key={item.continent}>
+            <>
+              <p>{item.continent}</p>
+              <p>
+                {item.active}
+                {' '}
+                active cases
+              </p>
+              <p>{new Date(item.updated).toDateString()}</p>
+              <button
+                type="button"
+                onClick={() => {
+                  navigate(`details/${item.continent}`);
+                }}
+              >
+                {item.continent}
+              </button>
+            </>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
