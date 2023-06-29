17b0b42d83a6cb460e8ed63b58c4acf52c0402b6
andrianarivo <andrianarivo.david@gmail.com>
2023-06-29 18:17:51 +0300

    build continent items styling

--- src/components/ContinentItem.js
+++ src/components/ContinentItem.js
@@ -1,18 +1,49 @@
 import PropTypes from 'prop-types';
 import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import moment from 'moment';
+import africa from '../images/africa.png';
+import europe from '../images/europe.png';
+import northAmerica from '../images/north_america.png';
+import asia from '../images/asia.png';
+import oceania from '../images/oceania.png';
+import southAmerica from '../images/south_america.png';
+
+const getImage = (continent) => {
+  switch (continent.toLowerCase()) {
+    case 'asia':
+      return asia;
+    case 'africa':
+      return africa;
+    case 'north america':
+      return northAmerica;
+    case 'south america':
+      return southAmerica;
+    case 'australia-oceania':
+      return oceania;
+    case 'europe':
+      return europe;
+    default:
+      return asia;
+  }
+};
 
 export default function ContinentItem(props) {
   const navigate = useNavigate();
   const { continent, active, updated } = props;
+
   return (
-    <li>
+    <Continent image={continent}>
       <p>{continent}</p>
       <p>
-        {active}
+        <span>{active}</span>
         {' '}
-        active cases
+        <span>active cases</span>
       </p>
-      <p>{new Date(updated).toDateString()}</p>
+      <p>
+        updated
+        {` ${moment(new Date(updated)).fromNow()}`}
+      </p>
       <button
         type="button"
         data-testid={`button-${continent}`}
@@ -20,9 +51,9 @@ export default function ContinentItem(props) {
           navigate(`details/${continent}`);
         }}
       >
-        {continent}
+        {' '}
       </button>
-    </li>
+    </Continent>
   );
 }
 
@@ -31,3 +62,60 @@ ContinentItem.propTypes = {
   active: PropTypes.number.isRequired,
   updated: PropTypes.number.isRequired,
 };
+
+const Continent = styled.li`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: flex-end;
+  position: relative;
+  list-style: none;
+  flex: 1 1 50%;
+  padding: 1rem 0;
+  height: 10rem;
+  background-image: url(${(props) => getImage(props.image)});
+  background-size: contain;
+  background-repeat: no-repeat;
+  filter: blur(5);
+
+  button {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+  }
+
+  p {
+    padding-right: 0.5rem;
+
+    span:nth-child(2) {
+      font-size: 0.7rem;
+    }
+  }
+
+  p:nth-child(1) {
+    font-weight: bold;
+    text-transform: uppercase;
+    text-align: end;
+  }
+
+  p:nth-child(3) {
+    font-size: 0.7rem;
+  }
+
+  &:nth-child(n) {
+    background-color: #dc4781;
+  }
+
+  &:nth-child(3n) {
+    background-color: #cf4378;
+  }
+
+  &:nth-child(2) {
+    background-color: #cf4378;
+  }
+`;
---
+++ src/components/FeaturedContinent.js
@@ -0,0 +1,7 @@
+export default function FeaturedContinent() {
+  return (
+    <div>
+      featured
+    </div>
+  );
+}
